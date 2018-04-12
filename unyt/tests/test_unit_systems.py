f27fd56cd3f351605aafe56e13449048543559b1
John ZuHone <jzuhone@gmail.com>
2018-04-12 12:16:07 -0400

    Fix test

--- unyt/tests/test_unit_systems.py
+++ unyt/tests/test_unit_systems.py
@@ -33,7 +33,9 @@ from unyt import (
 def test_unit_systems():
     goofy_unit_system = UnitSystem(
         "goofy", "ly", "lbm", "hr", temperature_unit="R",
-        angle_unit="arcsec", current_mks_unit="mA")
+        angle_unit="arcsec", mks_system=True,
+        current_mks_unit="mA", amount_unit="mol",
+        luminous_intensity_unit="cd")
     assert goofy_unit_system["temperature"] == Unit("R")
     assert goofy_unit_system[dimensions.solid_angle] == Unit("arcsec**2")
     assert goofy_unit_system["energy"] == Unit("lbm*ly**2/hr**2")
