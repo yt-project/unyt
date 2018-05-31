b9c5c05d36b08add2e6753f9970c7e630d53d6e8
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-05-31 13:21:40 -0500

    more plot tweaks, add a few more plots

--- paper/benchmark_plot.py
+++ paper/benchmark_plot.py
@@ -4,10 +4,10 @@ from collections import OrderedDict
 from matplotlib import pyplot as plt
 from matplotlib.patches import Patch
 
-HATCH_MAP = {'small': '\\\\\\', 'medium': '...', 'big': 'xxx'}
+ALPHA_MAP = {'small': 0.3333, 'medium': 0.666, 'big': 1.0}
 COLOR_MAP = {'unyt': 'C0', 'astropy': 'C1', 'pint': 'C2'}
-SIZE_LABELS = {'small': '3 elements', 'medium': '1,000 elements',
-               'big': '1,000,000 elements'}
+SIZE_LABELS = {'small': '3', 'medium': '$10^3$',
+               'big': '$10^6$'}
 
 
 def make_plot(benchmark_name, benchmarks, fig_filename):
@@ -49,30 +49,31 @@ def make_plot(benchmark_name, benchmarks, fig_filename):
                     (np_stddev/np_mean)**2 + (stddev/mean)**2)
                 ytick = ind + 1 + size_offset + package_offset + static_offset
                 ax.barh(ytick, ratios[package], width, xerr=stddevs[package],
-                        color=COLOR_MAP[package], label=package,
-                        hatch=HATCH_MAP[size])
+                        color=COLOR_MAP[package],
+                        label=' '.join([package, SIZE_LABELS[size]]),
+                        alpha=ALPHA_MAP[size])
                 all_yticks.append(ytick)
                 if package == 'astropy' and size == 'medium':
                     yticks.append(ytick)
             static_offset += .01
 
-    size_patches = [Patch(hatch=h, fc='w', ec='k') for h in HATCH_MAP.values()]
-    size_labels = [SIZE_LABELS[h] for h in HATCH_MAP.keys()]
-    package_patches = [Patch(color=c) for c in COLOR_MAP.values()]
-    leg = ax.legend(package_patches, COLOR_MAP.keys(), loc=1)
-    ax.legend(size_patches, size_labels, loc=4)
+    legend_patches = [Patch(color=c) for c in COLOR_MAP.values()]
+    leg = ax.legend(legend_patches, COLOR_MAP.keys(), loc=1)
+    size_patches = [Patch(color='k', alpha=a) for a in ALPHA_MAP.values()]
+    size_labels = [SIZE_LABELS[l] for l in ALPHA_MAP.keys()]
+    ax.legend(size_patches, size_labels, loc=4, title='Number of elements')
     ax.add_artist(leg)
     ax.set_xlabel(r'$T_{\rm package} / T_{\rm numpy}$')
     ax.set_xscale('symlog', linthresh=1)
-    ax.set_xlim(0, 450)
+    ax.set_xlim(0, 1000)
     ax.set_yticks(yticks)
     ax.set_xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
                    2, 3, 4, 5, 6, 7, 8, 9,
                    20, 30, 40, 50, 60, 70, 80, 90,
-                   200, 300, 400],
+                   200, 300, 400, 500, 600, 700, 800, 900],
                   minor=True)
-    ax.set_xticks([0, 1, 10, 100])
-    ax.set_xticklabels(['0', '1', '10', '100'])
+    ax.set_xticks([0, 1, 10, 100, 1000])
+    ax.set_xticklabels(['0', '1', '10', '100', '1000'])
     ax.set_yticklabels(benchmarks.values())
     ax.plot([1, 1], [0, len(benchmarks)+1], '--', color='k', lw=0.75,
             alpha=0.5)
@@ -96,10 +97,60 @@ make_plot(
 make_plot(
     benchmark_name='Unary operations',
     benchmarks={
-        'npsqrt': 'np.sqrt(data)',
         'sqrt': '(data)**(0.5)',
-        'npsquare': 'np.power(data, 2)',
         'square': 'data**2',
     },
     fig_filename='unary.png'
+)
+
+make_plot(
+    benchmark_name='Binary operations, different units',
+    benchmarks={
+        'kgg_operator.add12': 'a + b',
+        'kgg_operator.sub12': 'a - b',
+        'kgg_operator.mul12': 'a * b',
+        'kgg_operator.truediv12': 'a / b',
+        'kgg_operator.eq12': 'a == b',
+    },
+    fig_filename='binary_different_units.png'
+)
+
+make_plot(
+    benchmark_name='Binary operations, same units',
+    benchmarks={
+        'gg_operator.add12': 'a + b',
+        'gg_operator.sub12': 'a - b',
+        'gg_operator.mul12': 'a * b',
+        'gg_operator.truediv12': 'a / b',
+        'gg_operator.eq12': 'a == b',
+    },
+    fig_filename='binary_different_units.png'
+)
+
+make_plot(
+    benchmark_name="NumPy ufunc",
+    benchmarks={
+        'kgg_np.add12': 'np.add(a, b)',
+        'kgg_np.subtract12': 'np.add(a, b)',
+        'kgg_np.multiply12': 'np.multiply(a, b)',
+        'kgg_np.true_divide12': 'np.divide(a, b)',
+        'kgg_np.equal12': 'np.equal(a, b)',
+        'npsqrt': 'np.sqrt(data)',
+        'npsquare': 'np.power(data, 2)',
+    },
+    fig_filename='ufunc.png'
+)
+
+make_plot(
+    benchmark_name="In-place ufunc",
+    benchmarks={
+        'kgg_np.add12out': 'np.add(a, b, out=out)',
+        'kgg_np.subtract12out': 'np.add(a, b, out=out)',
+        'kgg_np.multiply12out': 'np.multiply(a, b, out=out)',
+        'kgg_np.true_divide12out': 'np.divide(a, b, out=out)',
+        'kgg_np.equal12out': 'np.equal(a, b, out=out)',
+        'npsqrtout': 'np.sqrt(data, out=out)',
+        'npsquareout': 'np.power(data, 2, out=out)',
+    },
+    fig_filename='ufuncout.png'
 )
