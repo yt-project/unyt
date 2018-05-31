84e9eae85b169837dc4565a76a3b4186b3ffe5e6
Nathan Goldbaum <ngoldbau@illinois.edu>
2018-05-31 00:54:47 -0500

    tweak plots

--- paper/benchmark_plot.py
+++ paper/benchmark_plot.py
@@ -6,6 +6,8 @@ from matplotlib.patches import Patch
 
 HATCH_MAP = {'small': '\\\\\\', 'medium': '...', 'big': 'xxx'}
 COLOR_MAP = {'unyt': 'C0', 'astropy': 'C1', 'pint': 'C2'}
+SIZE_LABELS = {'small': '3 elements', 'medium': '1,000 elements',
+               'big': '1,000,000 elements'}
 
 
 def make_plot(benchmark_name, benchmarks, fig_filename):
@@ -22,7 +24,7 @@ def make_plot(benchmark_name, benchmarks, fig_filename):
     package_offsets = [-width, 0, width]
     size_offsets = [-width*3, 0, width*3]
     static_offset = 0
-    sizes = ['small', 'medium', 'big']
+    sizes = ['big', 'medium', 'small']
     yticks = []
     all_yticks = []
 
@@ -55,19 +57,21 @@ def make_plot(benchmark_name, benchmarks, fig_filename):
             static_offset += .01
 
     size_patches = [Patch(hatch=h, fc='w', ec='k') for h in HATCH_MAP.values()]
+    size_labels = [SIZE_LABELS[h] for h in HATCH_MAP.keys()]
     package_patches = [Patch(color=c) for c in COLOR_MAP.values()]
-    leg = ax.legend(package_patches, COLOR_MAP.keys(), loc=4)
-    ax.legend(size_patches, HATCH_MAP.keys())
+    leg = ax.legend(package_patches, COLOR_MAP.keys(), loc=1)
+    ax.legend(size_patches, size_labels, loc=4)
     ax.add_artist(leg)
     ax.set_xlabel(r'$T_{\rm package} / T_{\rm numpy}$')
     ax.set_xscale('symlog', linthresh=1)
-    ax.set_xlim(0, 100)
+    ax.set_xlim(0, 450)
     ax.set_yticks(yticks)
     ax.set_xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
                    2, 3, 4, 5, 6, 7, 8, 9,
-                   20, 30, 40, 50, 60, 70, 80, 90],
+                   20, 30, 40, 50, 60, 70, 80, 90,
+                   200, 300, 400],
                   minor=True)
-    ax.set_xticks([0, 1, 10, 400])
+    ax.set_xticks([0, 1, 10, 100])
     ax.set_xticklabels(['0', '1', '10', '100'])
     ax.set_yticklabels(benchmarks.values())
     ax.plot([1, 1], [0, len(benchmarks)+1], '--', color='k', lw=0.75,
