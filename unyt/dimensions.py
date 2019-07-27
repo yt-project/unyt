d71edde2b36e249b577a5ff7095310a291a7137e
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-27 22:36:01 +0300

    Add decorator with docstrings

--- unyt/dimensions.py
+++ unyt/dimensions.py
@@ -13,7 +13,11 @@ Dimensions of physical quantities
 # -----------------------------------------------------------------------------
 
 
+from itertools import chain
+
 from sympy import Symbol, sympify, Rational
+from functools import wraps
+from unyt.exceptions import UnitOperationError
 
 #: mass
 mass = Symbol("(mass)", positive=True)
@@ -193,3 +197,98 @@ em_dimensions = {
 
 for k, v in list(em_dimensions.items()):
     em_dimensions[v] = k
+
+
+def check_dimensions(**arg_units):
+    """Decorator for checking units of function arguments.
+
+    Parameters
+    ----------
+    arg_units: dict
+        Mapping of function arguments to dimensions, of the form {'arg1': dimension1, etc},
+        where ``'arg1'`` etc are the function arguments and ``dimension1`` etc
+        is an SI base unit (or combination of units), eg. length/time.
+
+    Examples
+    --------
+    >>> import unyt as u
+    >>> from unyt.dimensions import length, time
+    >>> @check_dimensions(a=time, v=length/time)
+    ... def f(a, v):
+    ...     return a * v
+    ...
+    >>> res = f(a= 2 * u.s, v = 3 * u.m/u.s)
+    >>> print(res)
+    6 m
+    """
+    def check_nr_args(f):
+        """Ensure correct number of arguments and decorate.
+
+        Parameters
+        ----------
+        f : function
+            Function being decorated.
+
+        Returns
+        -------
+        new_f: function
+            Decorated function.
+
+        """
+        number_of_args = f.__code__.co_argcount
+        names_of_args = f.__code__.co_varnames
+
+        assert (
+            len(arg_units) == number_of_args
+        ), f'decorator number of arguments not equal with function number of arguments in "{f.__name__}"'
+
+        @wraps(f)
+        def new_f(*args, **kwargs):
+            """The new function being returned from the decorator.
+
+            Check units of `args` and `kwargs`, then run original function.
+
+            Raises
+            ------
+            UnitOperationError
+                If the units do not match.
+
+            """
+            for arg_name, arg_value in chain(zip(names_of_args, args), kwargs.items()):
+                dimension = arg_units[arg_name]
+                if arg_name in arg_units and not _has_units(arg_value, dimension):
+                    raise UnitOperationError(
+                        f"arg '{arg_name}'={repr(arg_value)} does not match {dimension}"
+                    )
+            return f(*args, **kwargs)
+        return new_f
+    return check_nr_args
+
+
+def _has_units(quant, dim):
+    """Checks the argument has the right dimensionality.
+
+    Parameters
+    ----------
+    quant : :py:class:`unyt.array.unyt_quantity`
+        Quantity whose dimensionality we want to check.
+    dim : :py:class:`sympy.core.symbol.Symbol`
+        SI base unit (or combination of units), eg. length/time
+
+    Returns
+    -------
+    bool
+        True if check successful.
+
+    Examples
+    --------
+    >>> import unyt as u
+    >>> from unyt.dimensions import length, time
+    >>> _has_units(3 * u.m/u.s, length/time)
+    True
+    """
+    try:
+        arg_dim = quant.units.dimensions
+    except AttributeError:
+        arg_dim = None
+    return arg_dim == dim
