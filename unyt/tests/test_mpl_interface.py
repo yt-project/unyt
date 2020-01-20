b40f275fbbde0d0606036891a92d4383e4900fd3
Lee Johnston <lee.johnston@ni.com>
2020-01-20 12:22:42 -0600

    rewrite of test_mpl_interface to address review comments

--- unyt/tests/test_mpl_interface.py
+++ unyt/tests/test_mpl_interface.py
@@ -1,17 +1,27 @@
 """Test Matplotlib ConversionInterface"""
-from pytest import raises
+import numpy as np
+import pytest
 from unyt._on_demand_imports import _matplotlib, NotAModule
 from unyt import s, K
 from unyt.exceptions import UnitConversionError
 
 
-def test_label():
-    if isinstance(_matplotlib.pyplot, NotAModule):
-        return
-    plt = _matplotlib.pyplot
+check_matplotlib = pytest.mark.skipif(
+    isinstance(_matplotlib.pyplot, NotAModule), reason="matplotlib not installed"
+)
+
+
+@pytest.fixture
+def ax(scope="module"):
+    yield _matplotlib.pyplot.figure().add_subplot()
+    _matplotlib.pyplot.close()
+
+
+@check_matplotlib
+def test_label(ax):
     x = [0, 1, 2] * s
     y = [3, 4, 5] * K
-    _, ax = plt.subplots()
+    ax.clear()
     ax.plot(x, y)
     expected_xlabel = "$\\left(\\rm{s}\\right)$"
     assert ax.xaxis.get_label().get_text() == expected_xlabel
@@ -19,13 +29,11 @@ def test_label():
     assert ax.yaxis.get_label().get_text() == expected_ylabel
 
 
-def test_convert_unit():
-    if isinstance(_matplotlib.pyplot, NotAModule):
-        return
-    plt = _matplotlib.pyplot
+@check_matplotlib
+def test_convert_unit(ax):
     x = [0, 1, 2] * s
     y = [1000, 2000, 3000] * K
-    _, ax = plt.subplots()
+    ax.clear()
     ax.plot(x, y, yunits="Celcius")
     expected = y.to("Celcius")
     line = ax.lines[0]
@@ -35,13 +43,11 @@ def test_convert_unit():
     assert results.all()
 
 
-def test_convert_equivalency():
-    if isinstance(_matplotlib.pyplot, NotAModule):
-        return
-    plt = _matplotlib.pyplot
+@check_matplotlib
+def test_convert_equivalency(ax):
     x = [0, 1, 2] * s
     y = [1000, 2000, 3000] * K
-    _, ax = plt.subplots()
+    ax.clear()
     ax.plot(x, y, yunits=("J", "thermal"))
     expected = y.to("J", "thermal")
     line = ax.lines[0]
@@ -51,26 +57,46 @@ def test_convert_equivalency():
     assert results.all()
 
 
-def test_dimensionless():
-    if isinstance(_matplotlib.pyplot, NotAModule):
-        return
-    plt = _matplotlib.pyplot
+@check_matplotlib
+def test_dimensionless(ax):
     x = [0, 1, 2] * s
     y = [3, 4, 5] * K / K
-    ax = plt.figure().add_subplot()
+    ax.clear()
     ax.plot(x, y)
     expected_ylabel = ""
     assert ax.yaxis.get_label().get_text() == expected_ylabel
 
 
-def test_conversionerror():
-    if isinstance(_matplotlib.pyplot, NotAModule):
-        return
-    plt = _matplotlib.pyplot
+@check_matplotlib
+def test_conversionerror(ax):
     x = [0, 1, 2] * s
     y = [3, 4, 5] * K
-    ax = plt.figure().add_subplot()
+    ax.clear()
     ax.plot(x, y)
     ax.xaxis.callbacks.exception_handler = None
-    with raises((_matplotlib.units.ConversionError, UnitConversionError)):
+    with pytest.raises((_matplotlib.units.ConversionError, UnitConversionError)):
         ax.xaxis.set_units("V")
+
+
+@check_matplotlib
+def test_ndarray_label(ax):
+    x = [0, 1, 2] * s
+    y = np.arange(3, 6)
+    ax.clear()
+    ax.plot(x, y)
+    expected_xlabel = "$\\left(\\rm{s}\\right)$"
+    assert ax.xaxis.get_label().get_text() == expected_xlabel
+    expected_ylabel = ""
+    assert ax.yaxis.get_label().get_text() == expected_ylabel
+
+
+@check_matplotlib
+def test_list_label(ax):
+    x = [0, 1, 2] * s
+    y = [3, 4, 5]
+    ax.clear()
+    ax.plot(x, y)
+    expected_xlabel = "$\\left(\\rm{s}\\right)$"
+    assert ax.xaxis.get_label().get_text() == expected_xlabel
+    expected_ylabel = ""
+    assert ax.yaxis.get_label().get_text() == expected_ylabel
