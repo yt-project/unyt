FIX: fix a bug where np.linalg.inv would incorrectly preserve units instead of inverting them (specialize the function via NEP 18)

unyt/tests/test_array_functions.py
@@ -1,9 +1,15 @@
 # tests for NEP 18
-from numpy import array_repr
+import numpy as np
 
 from unyt import cm
 
 
 def test_array_repr():
     arr = [1, 2, 3] * cm
-    assert array_repr(arr) == "unyt_array([1, 2, 3] cm)"
+    assert np.array_repr(arr) == "unyt_array([1, 2, 3] cm)"
+
+
+def test_linalg_inv():
+    arr = np.random.random_sample((3, 3)) * cm
+    iarr = np.linalg.inv(arr)
+    assert 1 * iarr.units == 1 / cm