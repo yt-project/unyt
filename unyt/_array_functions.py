df389b4e4b9ab5fa95dd94a73bf293136bc1aaaf
Clément Robert <cr52@protonmail.com>
2021-08-19 23:27:15 +0200

    FIX: add support for numpy.histogram FIX: add support for numpy.histogram, numpy.histogram2d and numpy.histogramdd

--- unyt/_array_functions.py
+++ unyt/_array_functions.py
@@ -21,3 +21,85 @@ def array2string(a, *args, **kwargs):
 @implements(np.linalg.inv)
 def linalg_inv(a, *args, **kwargs):
     return np.linalg.inv._implementation(a, *args, **kwargs).ndview / a.units
+
+
+def _sanitize_range(_range, units):
+    ndim = len(units)
+    if _range is None:
+        return _range
+    new_range = np.empty((ndim, 2))
+    for i in range(ndim):
+        ilim = _range[2 * i : 2 * (i + 1)]
+        imin, imax = ilim
+        if not (hasattr(imin, "units") and hasattr(imax, "units")):
+            raise TypeError(
+                f"Elements of range must both have a 'units' attribute. Got {_range}"
+            )
+        new_range[i] = imin.to(units[i]).value, imax.to(units[i]).value
+    return new_range.squeeze()
+
+
+@implements(np.histogram)
+def histogram(
+    a,
+    bins=10,
+    range=None,
+    normed=None,
+    weights=None,
+    density=None,
+):
+    range = _sanitize_range(range, units=[a.units])
+    counts, bins = np.histogram._implementation(
+        a.ndview,
+        bins=bins,
+        range=range,
+        normed=normed,
+        weights=weights,
+        density=density,
+    )
+    return counts, bins * a.units
+
+
+@implements(np.histogram2d)
+def histogram2d(
+    x,
+    y,
+    bins=10,
+    range=None,
+    normed=None,
+    weights=None,
+    density=None,
+):
+    range = _sanitize_range(range, units=[x.units, y.units])
+    counts, xbins, ybins = np.histogram2d._implementation(
+        x.ndview,
+        y.ndview,
+        bins=bins,
+        range=range,
+        normed=normed,
+        weights=weights,
+        density=density,
+    )
+    return counts, xbins * x.units, ybins * y.units
+
+
+@implements(np.histogramdd)
+def histogramdd(
+    sample,
+    bins=10,
+    range=None,
+    normed=None,
+    weights=None,
+    density=None,
+):
+    units = [_.units for _ in sample]
+    range = _sanitize_range(range, units=units)
+    counts, bins = np.histogramdd._implementation(
+        [_.ndview for _ in sample],
+        bins=bins,
+        range=range,
+        normed=normed,
+        weights=weights,
+        density=density,
+    )
+    return counts, tuple(_bin * u for _bin, u in zip(bins, units))
--- unyt/tests/test_array_functions.py
+++ unyt/tests/test_array_functions.py
@@ -1,7 +1,7 @@
 # tests for NEP 18
 import numpy as np
 
-from unyt import cm
+from unyt import cm, s, g
 
 
 def test_array_repr():
@@ -13,3 +13,30 @@ def test_linalg_inv():
     arr = np.random.random_sample((3, 3)) * cm
     iarr = np.linalg.inv(arr)
     assert 1 * iarr.units == 1 / cm
+
+
+def test_histogram():
+    arr = np.random.normal(size=1000) * cm
+    counts, bins = np.histogram(arr, bins=10, range=(arr.min(), arr.max()))
+    assert type(counts) is np.ndarray
+    assert bins.units == arr.units
+
+
+def test_histogram2d():
+    x = np.random.normal(size=100) * cm
+    y = np.random.normal(loc=10, size=100) * s
+    counts, xbins, ybins = np.histogram2d(x, y)
+    assert counts.ndim == 2
+    assert 1 * xbins.units == 1 * x.units
+    assert 1 * ybins.units == 1 * y.units
+
+
+def test_histogramdd():
+    x = np.random.normal(size=100) * cm
+    y = np.random.normal(size=100) * s
+    z = np.random.normal(size=100) * g
+    counts, (xbins, ybins, zbins) = np.histogramdd((x, y, z))
+    assert counts.ndim == 3
+    assert 1 * xbins.units == 1 * x.units
+    assert 1 * ybins.units == 1 * y.units
+    assert 1 * zbins.units == 1 * z.units
