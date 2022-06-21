003df5debde86f5c5a0fac87de5406f9c9ee667e
John ZuHone <jzuhone@gmail.com>
2022-06-21 09:46:10 -0400

    kill off the test error

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"unyt"
-copyright = u"2018, The yt Project"
+copyright = u"2022, The yt Project"
 author = u"The yt Project"
 
 # The version info for the project you're documenting, acts as replacement
@@ -72,7 +72,7 @@ release = unyt.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
